588754b6173f391608a91d29dbe9a4f20b98cdf7
Vivus Ignis <mail2ignis@gmail.com>
2014-05-24 22:56:33 +0200

    service testing stubs

--- spec_service_test.go
+++ spec_service_test.go
@@ -1,8 +1,30 @@
 package libnodespec
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
+
+func __initTestSpecServiceExisting() error {
+	fdScript, err := os.Create("/tmp/libnodespec_test.sh")
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintln(fdScript, "#!/bin/sh")
+	fmt.Fprintln(fdScript, "while :; do sleep 1; done")
+
+	if err := fdScript.Close(); err != nil {
+		return err
+	}
+
+	if err := os.Chmod("/tmp/libnodespec_test.sh", 755); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func TestSpecServiceNonexistent(t *testing.T) {
 	var testSpec SpecService
@@ -12,13 +34,16 @@ func TestSpecServiceNonexistent(t *testing.T) {
 	if err := testSpec.Run(gatherPlatformFacts()); err == nil {
 		t.Fatal(err)
 	}
-
 }
 
 func TestSpecServiceExisting(t *testing.T) {
 	var testSpec SpecService
 
-	testSpec.Name = "cron"
+	testSpec.Name = "libnodespec_test.sh"
+
+	if err := __initTestSpecServiceExisting(); err != nil {
+		t.Fatal(err)
+	}
 
 	if err := testSpec.Run(gatherPlatformFacts()); err != nil {
 		t.Fatal(err)
